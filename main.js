ed96fccb36995fbb656c553ccf7aa1f222d202a7
Shubh <shubhv2@gmail.com>
2022-03-26 14:12:42 -0400

    initial submission release 1

--- main.js
+++ main.js
@@ -1,19 +1,1 @@
-
-const allMenuItems = document.getElementById('personalMenu');
-
-const createMenuItem = s => {
-    const p = document.createElement('div');
-    p.setAttribute('class', 'grid-item-b');
-    // p.setAttribute('id', 'box');
-    p.textContent = s;
-    // p.addEventListener('click', () => printMe(i, j));
-    // console.log(p)
-    return p
-}
 // console.log(allMenuItems)
-allMenuItems.setAttribute('class', "personalMenu")
-allMenuItems.appendChild(createMenuItem("Calendar"));
-allMenuItems.appendChild(createMenuItem("Major Requirements"));
-allMenuItems.appendChild(createMenuItem("Clubs/RSOs"));
-allMenuItems.appendChild(createMenuItem("Research"));
-allMenuItems.appendChild(createMenuItem("Career Development"));
